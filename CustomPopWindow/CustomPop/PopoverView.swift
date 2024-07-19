e30888407e954ee962cb3cb6b77b9026f8c0a4bb
huangjunrong <jrlingdu888@gmail.com>
2024-07-19 14:00:52 +0800

    Update direction position

--- CustomPopWindow/CustomPop/PopoverView.swift
+++ CustomPopWindow/CustomPop/PopoverView.swift
@@ -21,14 +21,38 @@ class PopoverView: UIView {
     var backView: UIView?
     
     private var _arrowOrigin:CGPoint = CGPoint.zero
-    private var _arrowWidth:CGFloat = 10.0              //defatult
+    
+    private var _padding_h:CGFloat = 10.0               //横向间距
+    private var _padding_v:CGFloat = 50.0               //竖向间距
+    
+    private var _arrowWidth:CGFloat = 15.0              //defatult
     private var _arrowHeight:CGFloat = 10.0             //defatult
     
     private var _contentWidth:CGFloat = CGFloat.zero
     private var _contentHeight:CGFloat = CGFloat.zero
     private var _direction:PopoverViewArrowDirection = .up
     
+    //可传入目标视图
+    init(targetView tarView:UIView,contentWidth contentW:CGFloat,contentHeight contentH:CGFloat,arrowDirection direction:PopoverViewArrowDirection) {
+        super.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+        
+        //计算出目标视图在全屏中的原点位置
+        var pointInScreenView = CGPoint.zero
+        if let superview = tarView.superview {
+            let pointInWindow = superview.convert(tarView.frame.origin, to: nil)
+            let pointInScreen = tarView.window?.convert(pointInWindow, to: UIScreen.main.coordinateSpace)
+            pointInScreenView = pointInScreen!
+        }
+        
+        _arrowOrigin = arrowOriginWithTargetView(tarView, pointInScreenView,direction)
+        _contentWidth = contentW
+        _contentHeight = contentH
+        _direction = direction
+        
+        setupView()
+    }
     
+    //可传入原始坐标
     init(arrowOrigin origin:CGPoint,contentWidth contentW:CGFloat,contentHeight contentH:CGFloat,arrowDirection direction:PopoverViewArrowDirection) {
         super.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
         
@@ -43,13 +67,24 @@ class PopoverView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     func setupView(){
         //背景颜色为无色
         backgroundColor = UIColor.clear
+        
+        //这里会判断视图的宽高是否已经超出屏幕了，如超出则做出相应的处理
+        if _contentWidth >= UIScreen.main.bounds.width {
+            _contentWidth = UIScreen.main.bounds.width - 20  //这里可自定义超出屏幕做多少的处理
+        }
+        if _contentHeight >= UIScreen.main.bounds.height {
+            _contentHeight = UIScreen.main.bounds.height - 100  //这里可自定义超出屏幕做多少的处理
+        }
         
         backView = UIView(frame: CGRect(x: _arrowOrigin.x, y: _arrowOrigin.y + _arrowHeight, width: _contentWidth, height: _contentHeight))
         backView?.center.x = _arrowOrigin.x
@@ -59,6 +94,35 @@ class PopoverView: UIView {
         
         addSubview(backView!)
     }
+    
+    //设置对应方向的箭头原点位置
+    private func arrowOriginWithTargetView(_ tarView:UIView,_ originPoint:CGPoint,_ direction:PopoverViewArrowDirection) -> CGPoint{
+        var currentArrowOriginPoint:CGPoint = CGPointZero
+        
+        if (direction == .up) {
+
+            currentArrowOriginPoint.x = originPoint.x + tarView.frame.width/2
+            currentArrowOriginPoint.y = originPoint.y + tarView.frame.height
+            
+        }else if (direction == .down) {
+            
+            currentArrowOriginPoint.x = originPoint.x + tarView.frame.width/2
+            currentArrowOriginPoint.y = originPoint.y
+            
+        }else if (direction == .left) {
+            
+            currentArrowOriginPoint.x = originPoint.x + tarView.frame.width
+            currentArrowOriginPoint.y = originPoint.y + tarView.frame.height/2
+            
+        }else if (direction == .right) {
+            
+            currentArrowOriginPoint.x = originPoint.x
+            currentArrowOriginPoint.y = originPoint.y + tarView.frame.height/2
+        }
+        
+        return currentArrowOriginPoint
+    }
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 
@@ -71,41 +135,108 @@ class PopoverView: UIView {
     
     override func draw(_ rect: CGRect) {
         guard let context = UIGraphicsGetCurrentContext() else { return }
-//        context.clear(rect)
-        
+
         let startX = _arrowOrigin.x
         let startY = _arrowOrigin.y
-        
-//        let path = CGMutablePath()
-        
+
+        // Define the arrow path
         if (_direction == .up) {
-            
-            // Define the arrow path
             
             context.move(to: CGPoint(x: startX, y: startY))
             context.addLine(to: CGPoint(x: startX + self._arrowWidth / 2, y: startY + self._arrowHeight))
             context.addLine(to: CGPoint(x: startX - self._arrowWidth / 2, y: startY + self._arrowHeight))
-//            path.closeSubpath()
+
+        }else if (_direction == .down) {
             
-            // Fill the arrow
-//            context.addPath(path)
+            context.move(to: CGPoint(x: startX, y: startY))
+            context.addLine(to: CGPoint(x: startX - self._arrowWidth / 2, y: startY - self._arrowHeight))
+            context.addLine(to: CGPoint(x: startX + self._arrowWidth / 2, y: startY - self._arrowHeight))
+            
+        }else if (_direction == .left) {
+            
+            context.move(to: CGPoint(x: startX, y: startY))
+            context.addLine(to: CGPoint(x: startX + self._arrowHeight, y: startY - self._arrowWidth/2))
+            context.addLine(to: CGPoint(x: startX + self._arrowHeight, y: startY + self._arrowWidth/2))
+          
+        }else if (_direction == .right) {
+          
+            context.move(to: CGPoint(x: startX, y: startY))
+            context.addLine(to: CGPoint(x: startX - self._arrowHeight, y: startY - self._arrowWidth/2))
+            context.addLine(to: CGPoint(x: startX - self._arrowHeight, y: startY + self._arrowWidth/2))
+            
         }
-        context.closePath()
         
-//        context.setFillColor(UIColor.purple.cgColor)
+        
+        context.closePath()
+
         backView?.backgroundColor?.setFill()
         backgroundColor?.setStroke()
-//        context.fillPath()
         context.drawPath(using: .fillStroke)
-        // Add shadow on the sides
-//        let shadowPath = UIBezierPath(cgPath: path)
-//        self.layer.shadowPath = shadowPath.cgPath
+
+        refreshBackViewFrame()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    
+    func refreshBackViewFrame(){
+        
+        guard let _backView = backView else { return }
+        
+        if (_direction == .up) {
+            _backView.frame = CGRect(x: _arrowOrigin.x, y: _arrowOrigin.y + _arrowHeight, width: _contentWidth, height: _contentHeight)
+            _backView.center.x = _arrowOrigin.x
+            
+            //判断backView是否超出屏幕，如何超出屏幕左边则设置backiew的左边距 10，同样如果超出右边屏幕，则 backView 的右边距 10
+            if _backView.frame.origin.x < _padding_h {
+                _backView.frame.origin.x = _padding_h
+            }
+            if _backView.frame.origin.x + _contentWidth > (UIScreen.main.bounds.width - _padding_h) {
+                _backView.frame.origin.x = UIScreen.main.bounds.width - _contentWidth - _padding_h
+            }
+            
+        }else if (_direction == .down) {
+            _backView.frame = CGRect(x: _arrowOrigin.x, y: _arrowOrigin.y - _arrowHeight - _contentHeight, width: _contentWidth, height: _contentHeight)
+            _backView.center.x = _arrowOrigin.x
+            
+            //判断backView是否超出屏幕，如何超出屏幕左边则设置backiew的左边距 10，同样如果超出右边屏幕，则 backView 的右边距 10
+            if _backView.frame.origin.x < _padding_h {
+                _backView.frame.origin.x = _padding_h
+            }
+            if _backView.frame.origin.x + _contentWidth > (UIScreen.main.bounds.width - _padding_h) {
+                _backView.frame.origin.x = UIScreen.main.bounds.width - _contentWidth - _padding_h
+            }
+            
+        }else if (_direction == .left) {
+            
+            _backView.frame = CGRect(x: _arrowOrigin.x + _arrowHeight, y: _arrowOrigin.y, width: _contentWidth, height: _contentHeight)
+            _backView.center.y = _arrowOrigin.y
+            
+            //判断backView是否超出屏幕，如何超出屏幕上边则设置backiew的上边距 50，同样如果超出下边屏幕，则 backView 的下边距 50
+            if _backView.frame.origin.y <= _padding_v {
+                _backView.frame.origin.y = _padding_v
+            }
+            if _backView.frame.origin.y + _contentHeight > (UIScreen.main.bounds.height - _padding_v) {
+                _backView.frame.origin.y = UIScreen.main.bounds.height - _contentHeight - _padding_v
+            }
+            
+        }else if (_direction == .right) {
+            _backView.frame = CGRect(x: _arrowOrigin.x - _arrowHeight - _contentWidth, y: _arrowOrigin.y, width: _contentWidth, height: _contentHeight)
+            _backView.center.y = _arrowOrigin.y
+            
+            //判断backView是否超出屏幕，如何超出屏幕上边则设置backiew的上边距 50，同样如果超出下边屏幕，则 backView 的下边距 50
+            if _backView.frame.origin.y <= _padding_v {
+                _backView.frame.origin.y = _padding_v
+            }
+            if _backView.frame.origin.y + _contentHeight > (UIScreen.main.bounds.height - _padding_v) {
+                _backView.frame.origin.y = UIScreen.main.bounds.height - _contentHeight - _padding_v
+            }
+        }
+        
     }
     
+    func setArrowX(_ x:CGFloat) {
+        self._arrowOrigin.x = x
+        self.setNeedsLayout()
+    }
 }
 
 extension PopoverView {
@@ -127,18 +258,17 @@ extension PopoverView {
         //动画效果弹出
         alpha = 0
 
-        if (_direction == .up) {
-            _backView.frame = CGRect(x: _arrowOrigin.x, y: _arrowOrigin.y + _arrowHeight, width: 0, height: 0)
-            UIView.animate(withDuration: 0.2, animations: { [weak self] in
-                self?.alpha = 1
-                _backView.frame = CGRect(x: self!._arrowOrigin.x, y: self!._arrowOrigin.y + self!._arrowHeight, width: self!._contentWidth, height: self!._contentHeight)
-                _backView.center.x = self!._arrowOrigin.x
-            }, completion: { _ in
-                for view in result {
-                    view.isHidden = false
-                }
-            })
-        }
+        _backView.frame = CGRect(x: _arrowOrigin.x, y: _arrowOrigin.y + _arrowHeight, width: 0, height: 0)
+        UIView.animate(withDuration: 0.2, animations: { [weak self] in
+            self?.alpha = 1
+            self?.refreshBackViewFrame()
+            
+        }, completion: { _ in
+            for view in result {
+                view.isHidden = false
+            }
+        })
+        
     }
     
     func dismiss() {
@@ -160,6 +290,10 @@ extension PopoverView {
         })
         
         
+    }
+    
+    func contentAddSubView(_ view: UIView) {
+        backView?.addSubview(view)
     }
     
 }
--- CustomPopWindow/ViewController.swift
+++ CustomPopWindow/ViewController.swift
@@ -7,37 +7,123 @@
 
 import UIKit
 
+enum ButtonTag : Int {
+    case RedTag = 10
+    case YellowTag = 11
+    case BlueTag = 12
+    case GreenTag = 13
+    case PinkTag = 14
+    case OrangeTag = 15
+}
+
+
 class ViewController: UIViewController {
 
-    var popView:PopoverView? = nil
+    private lazy var label: UILabel = {
+        let view = UILabel(frame: CGRectMake(10, 10, 170, 100))
+        view.text = "你好啊 Hello world！\n我很好，没关系\nI'm fine It doesn't matter\nNice to meet you!"
+        view.textColor = .white
+        view.numberOfLines = 0
+        view.font = .systemFont(ofSize: 15)
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let btn = UIButton(frame: CGRect(x: 100, y: 100, width: 50, height: 50))
-        btn.backgroundColor = .yellow
+        let btn = UIButton(frame: CGRect(x: 50, y: 100, width: 50, height: 50))
+        btn.backgroundColor = .red
+        btn.tag = ButtonTag.RedTag.rawValue
         btn.addTarget(self, action: #selector(btnClick(_:)), for: .touchUpInside)
         view.addSubview(btn)
         
         
+        let btn2 = UIButton(frame: CGRect(x: 50, y: 400, width: 50, height: 50))
+        btn2.backgroundColor = .yellow
+        btn2.tag = ButtonTag.YellowTag.rawValue
+        btn2.addTarget(self, action: #selector(btnClick(_:)), for: .touchUpInside)
+        view.addSubview(btn2)
+        
+        let btn3 = UIButton(frame: CGRect(x: 50, y: UIScreen.main.bounds.height - 150, width: 50, height: 50))
+        btn3.backgroundColor = .blue
+        btn3.tag = ButtonTag.BlueTag.rawValue
+        btn3.addTarget(self, action: #selector(btnClick(_:)), for: .touchUpInside)
+        view.addSubview(btn3)
+        
+        let btn4 = UIButton(frame: CGRect(x: UIScreen.main.bounds.width - 100, y: 100, width: 50, height: 50))
+        btn4.backgroundColor = .green
+        btn4.tag = ButtonTag.GreenTag.rawValue
+        btn4.addTarget(self, action: #selector(btnClick(_:)), for: .touchUpInside)
+        view.addSubview(btn4)
+        
+        let btn5 = UIButton(frame: CGRect(x: UIScreen.main.bounds.width - 100, y: 400, width: 50, height: 50))
+        btn5.backgroundColor = .systemPink
+        btn5.tag = ButtonTag.PinkTag.rawValue
+        btn5.addTarget(self, action: #selector(btnClick(_:)), for: .touchUpInside)
+        view.addSubview(btn5)
+        
+        let btn6 = UIButton(frame: CGRect(x: UIScreen.main.bounds.width - 100, y: UIScreen.main.bounds.height - 150, width: 50, height: 50))
+        btn6.backgroundColor = .orange
+        btn6.tag = ButtonTag.OrangeTag.rawValue
+        btn6.addTarget(self, action: #selector(btnClick(_:)), for: .touchUpInside)
+        view.addSubview(btn6)
     }
 
     @objc func btnClick(_ btn:UIButton) {
-        btn.isSelected = !btn.isSelected
-        
-        
-        
-        if popView == nil {
-            popView = PopoverView(arrowOrigin: CGPoint(x: 150, y: 200), contentWidth: 200, contentHeight: 200, arrowDirection: .up)
+
+        if btn.tag == ButtonTag.RedTag.rawValue {
+            let popView = PopoverView(targetView: btn, contentWidth: 200, contentHeight: 200, arrowDirection: .up)
+            //添加弹窗内容视图
+            popView.contentAddSubView(label)
+            
+            popView.showPopView()
+            
+        } else if btn.tag == ButtonTag.YellowTag.rawValue {
+            
+            let popView = PopoverView(targetView: btn, contentWidth: 200, contentHeight: 200, arrowDirection: .left)
+            //添加弹窗内容视图
+            popView.contentAddSubView(label)
+            
+            popView.showPopView()
+            
+        } else if btn.tag == ButtonTag.BlueTag.rawValue {
+            
+            let popView = PopoverView(targetView: btn, contentWidth: 200, contentHeight: 200, arrowDirection: .down)
+            //添加弹窗内容视图
+            popView.contentAddSubView(label)
+            
+            popView.showPopView()
+            
+        } else if btn.tag == ButtonTag.GreenTag.rawValue {
+            
+            let popView = PopoverView(targetView: btn, contentWidth: 200, contentHeight: 200, arrowDirection: .up)
+            //添加弹窗内容视图
+            popView.contentAddSubView(label)
+            
+            popView.showPopView()
+            
+        } else if btn.tag == ButtonTag.PinkTag.rawValue {
+            
+            let popView = PopoverView(targetView: btn, contentWidth: 200, contentHeight: 200, arrowDirection: .right)
+            //添加弹窗内容视图
+            popView.contentAddSubView(label)
+            
+            popView.showPopView()
+            
+        } else if btn.tag == ButtonTag.OrangeTag.rawValue {
+            
+            let popView = PopoverView(targetView: btn, contentWidth: 200, contentHeight: 200, arrowDirection: .down)
+            //添加弹窗内容视图
+            popView.contentAddSubView(label)
+            
+            popView.showPopView()
             
         }
         
-//        if btn.isSelected {
-            popView!.showPopView()
-//        }else{
-//            popView?.dismiss()
-//        }
+        
+
+        
         
         
     }
